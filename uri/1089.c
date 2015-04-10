26503fc1388881852ef0b6e5687eac9195d9329b
Gabriel Russo <gabriel@Gabriels-MacBook-Air.local>
2015-04-09 22:49:59 -0300

    1089 spoj correto

--- uri/1089.c
+++ uri/1089.c
@@ -2,39 +2,47 @@
 
 int main(){
 	
-	int freq[10000], sentido, n,i,pico = 0;
+	int freq[10001], sentido, n,i,pico = 0;
 
 	while(scanf("%d",&n) && n != 0){
 	
-		/*guardo os valores*/
+		/*guardo todos*/
 		for(i = 0; i < n; i++){
 			scanf("%d",&freq[i]);
 		}
 
-		/*vejo como começa*/
-		if(freq[n-1] < freq[0])
+		/*comparo o ultimo com o primeiro para ver como comeca*/
+		if(freq[n-1] > freq[0]){
+			sentido = 0;
+		} else {
 			sentido = 1;
-		else
-			sentido = 0;	
+		}
+
+		/*coloco o primeiro na ultima posicao p comparar no final*/
+		freq[n] = freq[0];
 
 		for(i = 0; i < n; i++){
-			
+
 			if(sentido){
-				if(freq[i+1] < freq[i]){
-					sentido = 0;
+
+				if(freq[i] > freq[i+1]){
 					pico++;
+					sentido = 0;
+				}
+
+			} else {
+
+				if(freq[i] < freq[i+1]){
+					pico++;
+					sentido = 1;
 				}
 			}
-			else{	
-				if(freq[i+1] > freq[i]){
-					sentido = 1;
-					pico++;
-				}
-			}	
 		}
 
 		printf("%d\n",pico);
 		pico = 0;
+
+
 	} 
 
 
