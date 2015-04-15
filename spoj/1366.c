275ffc5f9a5c6d7d62427bede7a335649e108e90
Gabriel Russo <gabriel@Gabriels-MacBook-Air.local>
2015-04-15 00:15:21 -0300

    spoj tv

---
+++ spoj/1366.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(){
+	
+	int m,n,matriz[1001][1001],i,j,x,y,horizontal,vertical,ok,teste = 1;
+
+	while(scanf("%d %d",&m,&n) && m != 0){
+
+		horizontal = vertical = 0;
+		ok = 1;
+
+		for(i = 0; i < m; i++){
+			for(j = 0; j < n; j++){
+				scanf("%d",&matriz[i][j]);
+			}
+		}
+
+		while(ok){
+			scanf("%d %d",&x,&y);
+			horizontal += x;
+			vertical += -y;
+			if(x == 0 && y == 0)
+				ok = 0;
+		}
+
+		if(horizontal < 0)
+			horizontal = m + horizontal;
+		if(vertical < 0)
+			vertical = n + vertical;
+
+		printf("Teste %d\n",teste);
+
+		for(i = 0; i < m; i++){
+			for(j = 0; j < n; j++){
+				printf("%d ",matriz[(i+vertical)%m][(j+horizontal)%n]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+
+
+		teste++;
+
+	}
+
+
+
+	return 0;
+}
