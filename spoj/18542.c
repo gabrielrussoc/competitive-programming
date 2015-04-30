9aecb6837a365a8dbd7682b52cedd047e9155b24
gabriel de russo e carmo <gabriel.carmo@hulk.cec.ime.usp.br>
2015-04-30 10:55:14 -0300

    tiro ao alvo

---
+++ spoj/18542.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define MAX 100000
+
+int main(){
+
+	int c,t,v[MAX],x,y,i,raio;
+	
+	scanf("%d %d",&c,&t);
+	
+	for(i = 0; i < c; i++)
+		scanf("%d",&v[i]);
+
+	for(i = 0; i < t; i++){
+		scanf("%d %d",&x,&y);
+		raio = 
+		
+	
+	return 0;
+}
