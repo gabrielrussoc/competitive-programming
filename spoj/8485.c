12c5a1edb551b1b5dbead05f71ccf4cd765d2604
Gabriel Russo <gabrielrcarmo@gmail.com>
2015-05-08 18:54:45 -0300

    prob

--- spoj/8485.c
+++ spoj/8485.c
@@ -3,78 +3,72 @@
 #include <math.h>
 #define MAX 505
 
+int v[MAX][MAX];
+
+int buscab(int i,int j,int lo, int hi,int p){
+    int mid = (lo+hi)/2;
+
+    if(hi == lo){
+        if(v[i+mid][j+mid] >= p) return mid;
+        else return -1; 
+    }   
+
+    if(v[i+mid][j+mid] >= p)
+        return buscab(i,j,mid,hi,p);
+    else return buscab(i,j,mid+1,hi,p);
+
+}
+
 int main(){
+
+    int i,j,m,n,q,k,l,u,res,hi,baixo,alto;
     
-    int v[MAX][MAX],i,j,m,n,q,k,l,u,mid,hi,lo;
-    int baixo,alto,tam,x,y,flag,resp;
-    
-    while(scanf("%d %d",&n,&m) && n != 0){
-        
+	while(scanf("%d %d",&n,&m) && n != 0){
         for(i = 0; i < n; i++){
-            for(j = 0; j < m; j++)
-                scanf("%d",&v[i][j]);
+    		for(j = 0; j < m; j++)
+        		scanf("%d",&v[i][j]);
         }
         
         scanf("%d",&q);
-        
+       
         for(k = 0; k < q; k++){
             scanf("%d %d",&l,&u);
-            resp = 0;
-            
+    
+            res = 0;
+
             for(i = 0; i < n; i++){
-                lo = 0;
-                hi = m - 1;
-                while(hi > lo){
-                    mid = (hi+lo)/2;
-                    
-                    if(v[i][mid] < l)
-                        lo = mid+1;
-                    else
-                        hi = mid;
+                
+                for(hi = 0; hi < MAX; hi++){
+                    if(i + hi == n - 1 || hi == m-1)
+                        break; 
                 }
-                if(v[i][lo] >= l){
-                    baixo = lo;
-                    
-                    lo = 0;
-                    hi = m - 1;
-                    
-                    while(hi > lo){
-                        mid = (hi+lo+1)/2;
-                        
-                        if(v[i][mid] > u)
-                            hi = mid - 1;
-                        else
-                            lo = mid;
-                    }
-                    
-                    if(v[i][lo] <= u){
-                        alto = lo;
-                        
-                        if(alto-baixo+1 >= n-i)
-                            tam = n-i;
-                        else
-                            tam = alto-baixo+1;
-                        
-                        for(x = resp+1; x <= tam; x++){
-                            flag = 1;
-                            
-                            for(y = baixo; y <= x && flag; y++){
-                                if(v[i][y] < l && v[i][y] > u)
-                                    flag = 0;
-                            }
-                            if(flag)
-                                resp = x;
-                            
-                        }
-                    }
+
+                baixo = buscab(i,0,0,hi,l);
+                alto = buscab(i,0,0,hi,u) - 1;
+                if(baixo >= 0 && alto >= 0){
+                    if(abs(i+baixo - j+alto) > res)
+                        res = i+baixo - j + alto;
                 }
             }
-            printf("%d\n",resp);
-        }
+            for(j = 0; j < m; j++){
+                for(hi = 0; hi < MAX; hi++){
+                    if(j + hi == m - 1 || hi == n - 1)
+                        break;
+                }
+                
+                baixo = buscab(0,j,0,hi,l);
+                alto = buscab(0,j,0,hi,u) - 1;
+                if(baixo >= 0 && alto >= 0){
+                    if(abs(i+baixo - j + alto) > res)
+                        res = i + baixo - j + alto;
+                }
+            }
+
+            printf("%d\n",res);
+        } 
         printf("-\n");
-        
-    }
-    
+	}
     
     return 0;
 }
+
