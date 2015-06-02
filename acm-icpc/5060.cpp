29525108d49a1db1df3b8fde73ea02f83126b08a
Gabriel Russo <gabrielrcarmo@gmail.com>
2015-06-01 21:09:27 -0300

    ronaldinho

--- acm-icpc/5060.cpp
+++ acm-icpc/5060.cpp
@@ -2,52 +2,81 @@
 #include <cstdio>
 #include <cmath>
 #include <cstdlib>
+#include <vector>
+#define pb push_back
 
 using namespace std;
 
 int main(){
 	
 	queue<int> q;
+	vector<int> adj[33000];
+	int p[33000];
+	int r[33000];
 	int t;
 
 	scanf("%d",&t);
 
-	while(t--){
+	while(t){
 
 		int n, k;
 		scanf("%d %d",&n,&k);
-		int a;
+		int f;
 
-		for(int i = 0; i < pow(2,n); i++){
-			scanf("%d",&a);
-			q.push(a);
+		for(int i = 1; i <= (1 << n); i++){
+			scanf("%d",&f);
+			q.push(i);
+			p[i] = r[i] = f;
 		}
+
+		for(int i = 0; i <= (1 << n); i++)
+			adj[i].clear();
 
 		while(q.size() > 1){
 
-			int c1, c2;
+			int i1, i2;
 
-			c1 = q.front();
+			i1 = q.front();
 			q.pop();
-			c2 = q.front();
+			i2 = q.front();
 			q.pop();
 
-			if(c2 >= c1){
-				int s = c2;
-				c2 -= c1;
-				c2 = min(s,c2+k);
-				q.push(c2);
+
+			if(p[i1] == p[i2]){
+				int y = min(i1,i2);
+				p[y] = min(r[y],k);
+				q.push(y);
+				adj[y].pb(max(i1,i2));
 			}
-			else{
-				int s = c1;
-				c1 -= c2;
-				c1 = min(s,c1+k);
-				q.push(c2);
+			else {
+				int a, b;
+				if(p[i1] > p[i2]){
+					a = i1;
+					b = i2;
+				}
+				else {
+					a = i2;
+					b = i1;
+				}
+				
+				p[a] = min(r[a],p[a] - p[b] + k);
+				q.push(a);
+				adj[a].pb(b);
+
 			}
+
+
 		}
+		
 
-		printf("%d\n",q.front());
+		int ans = q.front();
+		printf("%d\n",ans);
 
+		for(int i = 0; i < n; i++)
+			printf("%d%c",adj[ans][i],i == n - 1 ? '\n' : ' ');
+
+		q.pop();
+		t--;
 	}
 
 }
