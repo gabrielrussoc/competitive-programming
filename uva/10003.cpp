7bbe38771e8c22fd73b18aae8e40c6830344d407
Gabriel Russo <gabrielrcarmo@gmail.com>
2015-06-28 16:19:10 -0300

    incompleto

---
+++ uva/10003.cpp
@@ -0,0 +1,34 @@
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+#include <map>
+#include <climits>
+
+using namespace std;
+
+int l;
+int c[55];
+
+int pd(int k, int i){
+
+	if(m[] != -1) return 
+
+	
+
+}
+
+
+int main(){
+
+	while(scanf("%d",&l) && l != 0){
+		int n;
+		scanf("%d",&n);
+		for(int i = 0; i < n; i++)
+			scanf("%d",&c[i]);
+
+
+	}
+
+
+
+}
