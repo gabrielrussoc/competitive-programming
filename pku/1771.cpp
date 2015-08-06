191f6c27ecd693ec3cfc4bd6c777763d70a8bd57
Gabriel de Russo e Carmo <gabrielrc@ruby.linux.ime.usp.br>
2015-08-06 16:37:40 -0300

    vai passar um dia

--- pku/1771.cpp
+++ pku/1771.cpp
@@ -7,26 +7,25 @@
 using namespace std;
 
 int n, v[55];
-int memo[55][55][3];
+int memo[55][55];
 
-int pd(int fl, int i, int p){
+int pd(int f, int i){
 
 	if(i == n) return 0;
-	if(fl > 31) return 123456;
-	
-	if(memo[fl][i][p] != -1) return memo[fl][i][p];
+	if(f > 31) return 123456;
 
-	int r1,r2;
-	r1 = 4+pd(fl+1,i,0);
-	r2 = abs(v[i]-fl)*20 + pd(fl,i+1,1);
-	if(p == 0 && fl != 1) r2 += 10;
+	int ans;
+	for(int j = 0; j < n-i; j++){
+		int maxi = 0;
+		for(int k = i; k < j+i; k++)
+			maxi = max(maxi,abs(f-v[k])*20 + 4*f);
+		ans = min(maxi,10+pd(f+1,i+j));
+	}
 
-	int ans = min(r1,r2);
-
-
-	memo[fl][i][p] = ans;
+	memo[f][i] = ans;
 	return ans;
 }
+
 
 int main(){
 	
@@ -35,7 +34,7 @@ int main(){
 		for(int i = 0; i < n; i++)
 			scanf("%d",&v[i]);
 
-		int ans = pd(1,0,0);
+		int ans = pd(1,0);
 		printf("%d\n",ans-10);
 
 	}
