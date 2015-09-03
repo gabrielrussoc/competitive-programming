5f629328ebe5f63377e32e7cf16a515dc871969b
Gabriel Russo <gabrielrcarmo@gmail.com>
2015-09-02 22:23:50 -0300

    tem q passar esse rapaz

--- tobi/bomba.cpp
+++ tobi/bomba.cpp
@@ -13,44 +13,53 @@
 
 using namespace std;
 
-vector<int> adj[505];
-pii d[505];
-map<pii,bool> f;
+vector<pii> adj[505];
+int d[505];
+int f[505][505];
+int s, cont = 0;
 
 void bfs (int j) {
     queue<int> q;
     q.push(j);
-    d[j].ff = d[j].ss = 0;
-
+    d[j] = 0;
     while(q.size()){
-       int p = q.front();
-       q.pop(); 
-       int k = adj[j].size();
-       for(int i = 0; i < k; i++){
-           if(f[mp(p,adj[p][i])] && d[adj[p][i]].ff > d[p].ff +1){
-              q.push(i); 
-              d[i].ff = d[p].ff + 1;
-           }
-           if(f[mp(p,adj[p][i])] && d[adj[p][i]].ss > d[p].ss +1){
-              q.push(i); 
-              d[i].ss = d[p].ss + 1;
-           }
-       }
+       	int a = q.front();
+		if (a == s) break;
+       	q.pop(); 
+       	int k = adj[a].size();
+       	for(int i = 0; i < k; i++){
+           	int b,c;
+           	b = adj[a][i].ff;
+		   	c = adj[a][i].ss;
+			if(d[a]%3 == 0 && c){
+				if(d[b] == INT_MAX || d[b]%3 != (d[a] + 1)%3){
+					d[b] = d[a]+1;
+					q.push(b);
+				}   
+			} else if (d[a]%3 && !c){
+				if(d[b] == INT_MAX || d[b]%3 != (d[a] + 1)%3){
+					d[b] = d[a]+1;
+					q.push(b);
+				}   
+			}
+	   }
     }
 }
 
 int main(){
     for(int i = 0; i < 505; i++){
-        d[i].ff = d[i].ss = INT_MAX;
+        d[i] = INT_MAX;
     }
-    int n,e,s,m;
+    int n,e,m;
     scanf("%d %d %d %d",&n,&e,&s,&m);
 
     for(int i = 0; i < m; i++){
         int a,b,t;
         scanf("%d %d %d",&a,&b,&t);
-        adj[a].pb(b);
-        f[mp(a,b)] = t;
+        adj[a].pb(mp(b,t));
     }
+	bfs(e);
+	if(d[s] == INT_MAX) printf("*\n");
+	else printf("%d\n",d[s]);
 
 }
