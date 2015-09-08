f18f7dfc01080407df26f3682a3a25aadad78da8
Gabriel de Russo e Carmo <gabrielrc@python.linux.ime.usp.br>
2015-09-08 18:19:42 -0300

    tive q implementar tudo na mao nessa porra

--- tobi/cartografico.cpp
+++ tobi/cartografico.cpp
@@ -2,55 +2,41 @@
 #include <cstdio>
 #include <vector>
 #include <climits>
+#include <cstring>
 #define pb push_back
+const int N = 1000006;
 
 using namespace std;
 
-vector<int> adj[1000006];
-int d[1000006];
 int n;
 int maxi, ind;
+int vhead[N], to[2*N], next[2*N], es;
 
-void preenche(){
-    for(int i = 1; i <= n; i++)
-        d[i] = INT_MAX;
+
+void dfs(int v, int pai, int d){
+    if(d > maxi) maxi = d, ind = v;
+    for(int e = vhead[v]; e> 0; e = next[e])
+        if(to[e] != pai) dfs(to[e],v,d+1);
 }
 
-void bfs(int j){
-    queue<int> q;
-    q.push(j);
-    d[j] = maxi = 0;
-    int at, nxt;
-    while(q.size()){
-        at = q.front();
-        if(d[at] > maxi){
-            maxi = d[at];
-            ind = at;
-        }
-        q.pop();
-        int k = adj[at].size();
-        for(int i = 0; i < k; i++){
-            nxt = adj[at][i];
-            if(d[nxt] > d[at] + 1){
-                d[nxt] = d[at]+1;
-                q.push(nxt);
-            }
-        }
-    }
-}
 
 int main(){
     int a,b;
+    es = 1;
     scanf("%d",&n);
     for(int i = 0; i < n-1; i++){
         scanf("%d %d",&a,&b);
-        d[i+1] = INT_MAX;
-        adj[a].pb(b);
-        adj[b].pb(a);
+        to[es] = b;
+        next[es] = vhead[a];
+        vhead[a] = es++;
+
+        to[es] = a;
+        next[es] = vhead[b];
+        vhead[b] = es++;
     }
-    d[n] = INT_MAX;
-    bfs(1);
-    preenche();
-    bfs(ind);
+    maxi = 0;
+    dfs(1,-1,0);
+    maxi = 0;
+    dfs(ind,-1,0);
     printf("%d\n",maxi);     
 }
