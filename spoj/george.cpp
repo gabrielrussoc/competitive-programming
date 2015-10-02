bed7aa159f4136d60761d6cea4af57de3887b362
Gabriel Russo <gabrielrcarmo@gmail.com>
2015-10-02 13:55:00 -0300

    ue

---
+++ spoj/george.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m,a,b,k,g;
+    scanf("%d %d",&n,&m);
+    scanf("%d %d %d %d",&a,&b,&k,&g);
+    scanf("%d",&foo);
+    t = 0;
+    for(int i = 0; i < g-1; i++){
+        scanf("%d",&bar);
+        forbid[foo][bar].ff = t;
+    }
+}
