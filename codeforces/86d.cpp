d97029bab757f74f8c994f3960698b5022d2ebc4
Gabriel Russo <gabrielrcarmo@gmail.com>
2015-10-27 23:09:41 -0200

    tl apertado mas passou

--- codeforces/86d.cpp
+++ codeforces/86d.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ans;
+typedef long long ll;
 //////////////0123456789
 const int N = 200005;
 int n, t, v[N], rz;
 ll freq[1000006];
 struct query{
     int l, r, i;
-    ll ans;
+    ll res;
 } q[N];
 
 ll ans;
@@ -21,56 +21,57 @@ bool comp2(query a, query b){
     return a.i < b.i;
 }
 
-void add(ll x){
+inline void add(ll x){
     ans -= freq[x]*freq[x]*x;
     freq[x]++;
     ans += freq[x]*freq[x]*x;
 }
 
-void del(ll x){
+inline void del(ll x){
     ans -= freq[x]*freq[x]*x;
     freq[x]--;
     ans += freq[x]*freq[x]*x;
 }
 
 int main(){
-    memset(freq,0,sizeof freq);
-    scanf("%d %d",&n,&t);
+    for(int i = 0; i < 1000001; i++) freq[i] = 0;
+    ios_base::sync_with_stdio(false);
+    cin >> n >> t;
     rz = sqrt(n);
     for(int i = 0; i < n; i++)
-        scanf("%d",v+i);
+        cin >> v[i];
     for(int i = 0; i < t; i++){
-        scanf("%d %d",&q[i].l,&q[i].r);
+        cin >> q[i].l >> q[i].r;
         q[i].i = i;
     }
     sort(q,q+t,comp);
     int cl, cr;
-    ll ans = 0;
+    ans = 0;
     int a,b;
     cl = cr = 0;
     add(v[0]);
     for(int i = 0; i < t; i++){
-        a = query[i].l;
-        b = query[i].r;
+        a = q[i].l - 1;
+        b = q[i].r - 1;
         while(cl < a){
             del(v[cl]);
             cl++;
         }
         while(cl > a){
-            add(v[cl]);
             cl--;
+            add(v[cl]);
         }
-        while(cr <= b){
-            add(v[cr]);
+        while(cr < b){
             cr++;
+            add(v[cr]);
         }
         while(cr > b){
             del(v[cr]);
             cr--;
         }
-        query[i].ans = ans;
+        q[i].res = ans;
     }
     sort(q,q+t,comp2);
     for(int i = 0; i < t; i++)
-        printf("%d\n",query[i].ans);
+        cout << q[i].res << '\n';
 }
