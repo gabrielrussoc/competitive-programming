73d6231d9c2d8b4e0d6ffe399133581112323cdf
Gabriel Russo <gabrielrcarmo@gmail.com>
2015-11-10 00:02:28 -0200

    fui dormir

--- uva/12486.cpp
+++ uva/12486.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 typedef long long ll;
 
-ll memo[20][20][12][2][2];
+ll memo[20][12][2][2];
 char str[24];
 
-ll dp(int i, int sum, int ult, int pre, int f){
-    if(str[i] == 0) return sum;
-    ll &me = memo[i][sum][ult][pre][f];
+ll dp(int i, int ult, int pre, int f){
+    if(str[i] == 0) return f;
+    ll &me = memo[i][ult][pre][f];
     if(me != -1) return me;
     me = 0;
     for(int d = 0; d < 10; d++){
         if(pre && d > str[i]-'0') break;
         int x = f;
         if(d == 4 || (d == 3 && ult == 1)) x = 1;
-        me += dp(i+1,x,d,pre && d == str[i]-'0',x);
+        me += dp(i+1,d,pre && d == str[i]-'0',x);
     }
     return me;
 }
@@ -24,6 +24,6 @@ int main(){
     while(scanf("%lld",&n) != EOF){
         memset(memo,-1,sizeof memo);
         sprintf(str,"%lld",n);
-        printf("%lld\n",n+dp(0,0,0,1,0));
+        printf("%lld\n",n-dp(0,0,1,0));
     }
 }
