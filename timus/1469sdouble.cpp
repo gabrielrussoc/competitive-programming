9a833779e525a38f61885d44dc57cded34532450
Gabriel Russo <gabrielrcarmo@gmail.com>
2016-02-04 12:27:14 -0200

    com double o bagulho eh louco

--- timus/1469sdouble.cpp
+++ timus/1469sdouble.cpp
@@ -13,12 +13,14 @@ typedef long double cood;
 const cood eps = 1e-9;
 const int inf = INT_MAX;
 //////////////0123456789
-const int N = 50005;
+const int N = 50004;
 const int modn = 1000000007;
-typedef complex<cood> point;
+typedef complex<ll> point;
 
 int ss, n;
-cood xg;
+const int A = 20011;
+const int B = 20021;
+int xg;
 int p[2*N];
 
 struct seg {
@@ -29,17 +31,16 @@ struct seg {
     seg (point a, int j, bool c): p(a), i(j), f(c){}
 } v[2*N];
 const cood PI = acos(-1);
-cood x (point a) { return a.real(); }
-cood y (point a) { return a.imag(); }
-cood cross (point a, point b){ return y(conj(a)*b); }
-point rot (point a, cood theta) { return a * polar(1.0L, theta); }
+ll x (point a) { return a.real(); }
+ll y (point a) { return a.imag(); }
+ll cross (point a, point b){ return y(conj(a)*b); }
 
 cood cur_y(int i){
-    return (xg-x(v[i<<1].p))*((y(v[i<<1|1].p)-y(v[i<<1].p))/(x(v[i<<1|1].p)-x(v[i<<1].p))) + y(v[i<<1].p); 
+    return y(v[i<<1].p) + cood(y(v[i<<1|1].p)-y(v[i<<1].p)) / cood(x(v[i<<1|1].p)-x(v[i<<1].p)) * cood(xg-x(v[i<<1].p)); 
 }
 
 bool comp(int i, int j) {
-    return x(v[i].p) < x(v[j].p)-eps;
+    return x(v[i].p) < x(v[j].p);
 }
 
 struct comp2 {
@@ -50,18 +51,21 @@ struct comp2 {
     }
 };
 
-bool int_ex (cood a, cood b, cood c, cood d) {
+bool int_ex (ll a, ll b, ll c, ll d) {
     if(a > b) swap(a,b);
     if(c > d) swap(c,d);
-    return max(a,c) <= min(b,d)+eps;
+    return max(a,c) <= min(b,d);
 }
 
-int sig(cood x){
-    return (x>-eps)-(x<eps);
+int sig(ll x){
+    return (x>0)-(x<0); 
 }
 
 bool intersect (point a, point b, point c, point d){
-    return (sig(cross(d-c,a-c))*sig(cross(d-c,b-c)) <= 0) && (sig(cross(b-a,c-a))*sig(cross(b-a,d-a)) <= 0) && int_ex(x(a),x(b),x(c),x(d)) && int_ex(y(a),y(b),y(c),y(d));
+    return (sig(cross(d-c,a-c))*sig(cross(d-c,b-c)) <= 0) 
+        && (sig(cross(b-a,c-a))*sig(cross(b-a,d-a)) <= 0) 
+        && int_ex(x(a),x(b),x(c),x(d)) 
+        && int_ex(y(a),y(b),y(c),y(d));
 }
 
 bool inter(int a, int b){
@@ -71,16 +75,14 @@ bool inter(int a, int b){
 int main() {
     set<int,comp2> s;
     set<int,comp2>::iterator it, it2;
-    srand(time(NULL));
     scanf("%d",&n);
-    cood ang = 2.L*PI*((1.L*rand())/(1.L*RAND_MAX));
     for(int i = 0; i < n; i++){
         int a,b,c,d;
         point p1, p2;
         scanf("%d %d %d %d",&a,&b,&c,&d);
-        p1 = rot(point(a,b), ang);
-        p2 = rot(point(c,d), ang);
-        if(x(p1) > x(p2)-eps) swap(p1,p2);
+        p1 = point(A*a + B*b, A*a - B*b);
+        p2 = point(A*c + B*d, A*c - B*d);
+        if(x(p1) > x(p2)) swap(p1,p2);
         v[ss++] = seg(p1,i,1);
         v[ss++] = seg(p2,i,0);
     }
