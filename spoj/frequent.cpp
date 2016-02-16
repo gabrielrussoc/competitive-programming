395e4dbe6da052e7c6c66c5904eca1b51e81fe14
Gabriel Russo <gabrielrcarmo@gmail.com>
2016-02-16 17:35:31 -0200

    izi

--- spoj/frequent.cpp
+++ spoj/frequent.cpp
@@ -1,54 +1,79 @@
-#include <cstdio>
-#include <map>
-#include <algorithm>
-#define MAXN 100005
-
+#include <bits/stdc++.h>
+#define mp make_pair
+#define pb push_back
+#define ff first
+#define ss second
 using namespace std;
 
+typedef long long ll;
 typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
 
-int n,q;
-int in[MAXN];
+const double eps = 1e-9;
+const int inf = INT_MAX;
+//////////////0123456789
+const int N = 100005;
+const int modn = 1000000007;
 
-struct node{
-	int m;
-	pii l;
-	pii r;
-} tree[4*MAXN];
+int n, q, a, b;
+int v[N];
 
-void update(int pos, int k, int val, int l, int r){
+struct no {
+    int pv, pq, sv, sq, mq;
+    no (){}
+    no (int a, int b, int c,int d, int e) : pv(a), pq(b), sv(c), sq(d), mq(e) {}
+} tree[4*N];
 
-	if(l == r){
-		tree[k].m = 1;
-		tree[k].l.first = val;
-		tree[k].l.second = 1;
-		tree[k].r.first = val;
-		tree[k].r.second = 1;
-	}
-	else{
-		int mid = (l+r)/2;
+no join (no a, no b){
+    no ret;
+    
+    // pre
+    ret.pv = a.pv; ret.pq = a.pq;
+    if(a.pv == b.pv) ret.pq = a.pq + b.pq;
 
-		if(mid <= pos)
-			update(pos,2*k,val,l,mid);
-		else
-			update(pos,2*k+1,val,mid+1,r);
+    //suf
+    ret.sv = b.sv; ret.sq = b.sq;
+    if(b.sv == a.sv) ret.sq = a.sq + b.sq;
 
-		if(tree[2*k].l.first == tree[2*k+1].r.first){
-			tree[k].m = tree[2*k].m + tree[2*k+1].m;
-			tree[k].l.first = in[l];
-			tree[k].r.first = in[r];
-			tree[k].l.second = 
-		}
-		
-	}
+    //resp
+    ret.mq = a.mq;
+    ret.mq = max(ret.mq,b.mq);
+    ret.mq = max(ret.mq,ret.sq);
+    ret.mq = max(ret.mq,ret.pq);
+    if(a.sv == b.pv) ret.mq = max(ret.mq, a.sq + b.pq);
+    return ret;
 }
 
-int main(){
-	
-	scanf("%d %d",&n,&q);
-	for(int i = 0; i < n; i++){
-		scanf("%d",&in[i]);
-		update(i,1,in[i],0,n-1);
-	}
-
+void build(int k, int l, int r) {
+    if(l == r) {
+        tree[k] = no(v[l],1,v[r],1,1);
+    }
+    else {
+        int m = (l+r)/2;
+        build(2*k,l,m);
+        build(2*k+1,m+1,r);
+        tree[k] = join(tree[2*k],tree[2*k+1]);
+    }
 }
+
+no query(int k, int l, int r, int fl, int fr){
+    if(l >= fl && r <= fr) return tree[k];
+    int m = (l+r)/2;
+    if(fl > m || fr < l) return query(2*k+1,m+1,r,fl,fr);
+    if(fr < m+1 || fl > r) return query(2*k,l,m,fl,fr); 
+    return join(query(2*k,l,m,fl,fr), query(2*k+1,m+1,r,fl,fr));
+}
+
+int main() {
+    while(scanf("%d",&n) && n){
+        scanf("%d",&q);
+        for(int i = 0; i < n; i++) scanf("%d",v+i);
+        build(1,0,n-1);
+        while(q--){
+            scanf("%d %d",&a,&b);
+            a--; b--;
+            printf("%d\n",query(1,0,n-1,a,b).mq);      
+        }
+    }
+}
+
