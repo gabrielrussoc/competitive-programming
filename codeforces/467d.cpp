01c3cd61f493e6e60c8f51f305a0a9bc67846cad
Gabriel Russo <gabrielrcarmo@gmail.com>
2016-02-29 23:11:07 -0300

    agora passou

--- codeforces/467d.cpp
+++ codeforces/467d.cpp
@@ -12,12 +12,12 @@ typedef pair<ll,ll> pll;
 const double eps = 1e-9;
 const int inf = INT_MAX;
 //////////////0123456789
-const int N = 1000004;
+const int N = 500004;
 const int modn = 1000000007;
 
 string ss;
 char s[N];
-pii ans[N], ans2[N];
+pll ans[N], ans2[N];
 map<string, int> m;
 vector<string> v;
 vector<int> adj[N];
@@ -59,59 +59,51 @@ void dfs(int u) {
     }
 }
 
+void lowerize() {
+    for(int j = 0; s[j]; j++) s[j] = tolower(s[j]);
+}
+
+void label() {
+    m[ss] = k++;
+    int cont = 0, j;
+    for(j = 0; s[j]; j++) if(s[j] == 'r') cont++;
+    ans[k-1] = pii(cont,j);
+}
 
 int main() {
     scanf("%d",&mm);
     for (int i = 0; i < mm; i++){
-        scanf(" %s", s);
-        for(int j = 0; s[j]; j++) s[j] = tolower(s[j]);
-        ss = s;
-        if(!m[ss]) {
-            m[ss] = k++;
-            int cont = 0, j;
-            for(j = 0; s[j]; j++) if(s[j] == 'r') cont++;
-            ans[k-1] = pii(cont,j);
-        }
+        scanf(" %s", s); lowerize(); ss = s;
+        if(!m[ss]) label();
         v.pb(ss);
     }
     scanf("%d",&n);
     int a,b;
     for(int i = 0; i < n; i++){
-        scanf(" %s", s); 
-        for(int j = 0; s[j]; j++) s[j] = tolower(s[j]);
-        ss = s;
-        if(!m[ss]) {
-            m[ss] = k++;
-            int cont = 0, j;
-            for(j = 0; s[j]; j++) if(s[j] == 'r') cont++;
-            ans[k-1] = pii(cont,j);
-        }
+        scanf(" %s", s); lowerize(); ss = s;
+        if(!m[ss]) label();
         a = m[ss];
-        scanf(" %s", s); 
-        for(int j = 0; s[j]; j++) s[j] = tolower(s[j]);
-        ss = s;
-        if(!m[ss]) {
-            m[ss] = k++;
-            int cont = 0, j;
-            for(j = 0; s[j]; j++) if(s[j] == 'r') cont++;
-            ans[k-1] = pii(cont,j);
-        }
+        
+        scanf(" %s", s); lowerize(); ss = s;
+        if(!m[ss]) label();
         b = m[ss];
+        
         adj[a].pb(b);
     }
-    for (int i = 1; i < k; i++) scc(i);
+    for (int i = 1; i < k; i++) if(!disc[i]) scc(i);
     for (int i = 0; i < sc; i++) ans2[i] = pii(inf,inf);
     for (int i = 1; i < k; i++) ans2[comp[i]] = min(ans2[comp[i]],ans[i]);
+
     for(int u = 1; u < k; u++) 
         for(int v : adj[u]) 
-            if(comp[u] != comp[v]) {
+            if(comp[u] != comp[v]) 
                 adj2[comp[u]].pb(comp[v]);
-            }
-    for(int u = 0; u < sc; u++)
-        dfs(u);
-    int t, r;
+    
+    for(int u = 0; u < sc; u++) if(!vis[u]) dfs(u);
+
+    ll t, r;
     t = r = 0;
     for(int i = 0; i < mm; i++) r += ans2[comp[m[v[i]]]].ff, t += ans2[comp[m[v[i]]]].ss; 
-    printf("%d %d\n",r,t);
+    cout << r << " " << t << endl;
 }
 
